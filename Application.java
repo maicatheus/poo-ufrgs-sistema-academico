ff661a3beb90ab0ccd0a7d0ccf53403d5b683a27
Matheus Maica <matheus_maica@sicredi.com.br>
2022-12-20 11:22:08 -0300

    Menu Inicial

--- Application.java
+++ Application.java
@@ -1,5 +1,17 @@
 public class Application {
     public static void main(String[] args) {
+        menuInicial();
+    }
+
+    public static void menuInicial(){
+        System.out.println("Escolha uma opcao:");
+        System.out.println("[1] - Cadastrar um curso");
+        System.out.println("[2] - Cadastrar uma disciplina");
+        System.out.println("[3] - Cadastrar uma oferta");
+        System.out.println("[4] - Excluir uma oferta");
+        System.out.println("[5] - Listar os cursos");
+        System.out.println("[6] - Listar o curriculo de um curso, agrupando as disciplinas de cada etapa");
+        System.out.println("[7] - Listar todas as ofertas de uma disciplina");
 
     }
 }

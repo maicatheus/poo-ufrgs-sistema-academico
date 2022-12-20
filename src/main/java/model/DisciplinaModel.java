34591ef6cfe86a1f8a9cd97367ae3c8f3fde3d54
Matheus Maica <matheus_maica@sicredi.com.br>
2022-12-20 09:25:31 -0300

    Adicionando DisciplinaModel CaraterEnum

---
+++ src/main/java/model/DisciplinaModel.java
@@ -0,0 +1,17 @@
+package src.main.java.model;
+
+import src.main.java.enums.CaraterEnum;
+
+public class DisciplinaModel {
+    private String codigo;
+    private String nome;
+    private Integer creditos;
+    private Integer cargaHoraria;
+    private CaraterEnum carater;
+
+    public DisciplinaModel(){
+    };
+
+
+
+}
